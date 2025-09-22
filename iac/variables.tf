cd914d4df820f32e23cba2085feb6af63cd57147
José Corrochano Pardo <josecorrochanop2003@gmail.com>
2025-09-22 14:17:00 +0200

    New update

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,28 +1,28 @@
 # General
 variable "location" { type = string }
-variable "rg_name"  { type = string }
+variable "rg_name" { type = string }
 
 # Compute VNet
 variable "vnet_compute_name" { type = string }
 variable "vnet_compute_cidr" { type = string }
-variable "subnet_db_name"    { type = string }
-variable "subnet_db_cidr"    { type = string }
+variable "subnet_db_name" { type = string }
+variable "subnet_db_cidr" { type = string }
 
 # AKS VNet
-variable "vnet_aks_name"    { type = string }
-variable "vnet_aks_cidr"    { type = string }
-variable "subnet_aks_name"  { type = string }
-variable "subnet_aks_cidr"  { type = string }
+variable "vnet_aks_name" { type = string }
+variable "vnet_aks_cidr" { type = string }
+variable "subnet_aks_name" { type = string }
+variable "subnet_aks_cidr" { type = string }
 
 # VMs
-variable "vm_db_name"       { type = string }
-variable "vm_bkp_name"      { type = string }
-variable "vm_admin_user"    { type = string }
-variable "ssh_public_key"   { type = string }
+variable "vm_db_name" { type = string }
+variable "vm_bkp_name" { type = string }
+variable "vm_admin_user" { type = string }
+variable "ssh_public_key" { type = string }
 
 # AKS
-variable "aks_name"         { type = string }
-variable "aks_node_count"   { type = number }
+variable "aks_name" { type = string }
+variable "aks_node_count" { type = number }
 variable "aks_node_vm_size" { type = string }
 
 
